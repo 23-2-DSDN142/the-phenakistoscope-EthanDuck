1574e373e86eea70d32c96cd8de68d7ec6c126de
Ethan Duck <ethan7.duck@gmail.com>
2023-08-17 09:27:29 +1200

    More spinny circle

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -9,7 +9,7 @@ function setup_pScope(pScope) {
   pScope.output_mode(animate ? ANIMATED_DISK : STATIC_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(true);
-  pScope.set_direction(CW);
+  pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
 
@@ -17,7 +17,7 @@ function setup_layers(pScope) {
   colorMode(HSL, circleCount);
   noStroke();
 
-  new PLayer(null, 0); //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 0);
 
   var layer1 = new PLayer(circles);
   layer1.mode(RING);
@@ -26,12 +26,12 @@ function setup_layers(pScope) {
 function circles(x, y, animation, pScope) {
   for (let i = 1; i < (circleCount + 1); i++) {
 
-    let x = (animation.frame * SLICE_COUNT * 5);
-    let y = i * (pieRadius / (circleCount));
+    let x = (animation.frame * SLICE_COUNT * 5) + 300;
+    let y = i * (pieRadius / circleCount);
 
-    let w = 50 * (circleCount - i) * 0.01 * (SLICE_COUNT - (animation.frame * SLICE_COUNT));
+    let w = (animation.wave() * SLICE_COUNT * i) + 10;
 
-    let h = ((animation.frame * SLICE_COUNT) + (i / 4)) % (circleCount);
+    let h = ((animation.frame * SLICE_COUNT) + (i / 4)) % circleCount;
     fill(h, circleCount, (circleCount / 2));
     ellipse(x, y, w, w);
   }
